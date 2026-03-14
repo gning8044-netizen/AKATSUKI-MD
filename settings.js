b2efeb810779f9f0916ccf4334cf7149de135564
Shadow prime <gning8044@gmail.com>
2026-03-14 16:33:11 +0000

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'AKATSUKI MD',
+  author: 'SHADOW',
+  botName: "AKATSUKI MD",
+  botOwner: 'SHADOW TECH', // Your name
+  ownerNumber: '221766297256', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.9",
+  updateZipUrl: "https://github.com/TECHGOAT333/LASER-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
